b9afd2cdb6f11f2964c063c020e783ecaf3ca70e
HITESH MEWADA <72666559+hiteshmewada@users.noreply.github.com>
2022-01-18 15:19:45 +0530

    Update _dbconnect.php

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -2,7 +2,7 @@
     // Script to connect to database
     $server="localhost33";
     $user="hitesh";
-    $pass="H112+6@h1";
+    $pass="";
     $db="iforum";
     $con=mysqli_connect($server,$user,$pass,$db);
     
