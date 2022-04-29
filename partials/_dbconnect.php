aac2f0e3f3e9533ba0e168344395fef06056274f
HITESH MEWADA <mewadahitesh00@gmail.com>
2022-04-29 16:28:49 +0530

    Delete partials directory

--- partials/_dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    // Script to connect to database
-    $server="localhost";
-    $user="root";
-    $pass="";
-    $db="iforum";
-    $con=mysqli_connect($server,$user,$pass,$db);
-?>
--- partials/_logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    echo "Logging you out.... Wait for some time.........";
-    session_destroy();
-    header('Location: /forum_in_php/index.php');
-?>
